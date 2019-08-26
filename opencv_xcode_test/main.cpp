2ad9514e5882a18d1ef79e3b9e04f173cad09841
Park Billy <rtlink.park@gmail.com>
2019-08-26 10:45:38 +0900

    환경 설정 및 버전 확인

---
+++ opencv_xcode_test/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  opencv_xcode_test
+//
+//  Created by Park Billy on 26/08/2019.
+//  Copyright © 2019 Park Billy. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char * argv[]) {
+
+    std::cout << "Hello, World!\n";
+    std::cout << "OpenCV Version : " << CV_VERSION << std::endl;
+    
+    return 0;
+}
